770f67ba95c22dc0734c2d78d555997a4761cb95
Aditya <102435771+fluxsigma@users.noreply.github.com>
2023-02-03 14:49:44 +0530

    w

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
+//cuz we all love themes so here it is blueish theme
+
+
+const themeButton = document.getElementById('theme-button')
+console.log(themeButton)
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    themeButton.classList.toggle("yellow")
+})
+
+
 
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
 
 // show menu
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 // menu hidden
-//Validate if constant exists
-if(navClose){
-    navClose.addEventListener('click', () =>{
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,7 +34,7 @@ if(navClose){
 //remove menu mob
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each navlink, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -31,25 +43,17 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // here i added what to do after we scroll on website after a certain point
 
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
-   
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 
 
 
-//cuz we all love themes so here it is dark-light theme
-const themeButton = document.getElementById('theme-button')
-console.log(themeButton)
 
-themeButton.addEventListener("click",()=>{
-document.body.classList.toggle("dark")
-themeButton.classList.toggle("yellow")
-})
 
-    
 
